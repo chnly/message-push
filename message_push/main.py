7f95abd720ec1cedb0ef86f33f8db9debc2b444c
simple <zhaogang@smart-lifestyle.cn>
2020-12-16 23:27:27 +0800

    feat: 三个推送服务接口（短信、微信、邮件）

---
+++ message_push/main.py
@@ -0,0 +1,63 @@
+from typing import List, Optional
+from fastapi import FastAPI, Response, status, Header, Depends, BackgroundTasks
+from models import EmailModel, SMSModel, WechatModel
+import uvicorn
+from fastapi.security.oauth2 import get_authorization_scheme_param
+from fastapi.exceptions import HTTPException
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
+from authorize import has_access
+from mail.mailbox import html_loader, email_sender, EmailTemplate
+from sms.smsbox import sms_sender, SMSTemplate
+from wechat.wxbox import wx_sender, WXTemplate
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"hello": "FastAPI"}
+
+
+@app.post("/api/v1/services/sms/messages", dependencies=[Depends(has_access)])
+async def push_sms_message(params: SMSModel, response: Response, background_tasks: BackgroundTasks):
+    params_dict = params.dict()
+    template_name = params_dict['template_name']
+    to_users = params_dict['to_users']
+    template_params = params_dict['message']
+    new_sms = SMSTemplate(to_users, template_params)
+    background_tasks.add_task(sms_sender.send, template_name, new_sms)
+    return "success"
+
+
+@app.post("/api/v1/services/email/messages", dependencies=[Depends(has_access)])
+async def push_email_message(params: EmailModel, response: Response, background_tasks: BackgroundTasks):
+    params_dict = params.dict()
+    template_name = params_dict['template_name']
+    subject = params_dict['subject']
+    to_users = params_dict['to_users']
+    cc_users = params_dict['cc_users']
+    priority = params_dict['priority']
+    template_params = params_dict['message']
+    html_file = template_name + ".html"
+    content = html_loader.render(html_file, **template_params)
+    sender = "digital.service@cn.abb.com"
+    new_email = EmailTemplate(subject, sender, to_users, cc_users, content)
+    background_tasks.add_task(email_sender.send, new_email)
+
+    return "success"
+
+
+@app.post("/api/v1/services/wechat/messages", dependencies=[Depends(has_access)])
+async def push_wechat_message(params: WechatModel, response: Response, background_tasks: BackgroundTasks):
+    params_dict = params.dict()
+    template_id = params_dict['template_id']
+    to_users = params_dict['to_users']
+    message = params_dict['message']
+    miniprogram = params_dict['miniprogram']
+    new_wx_messages = WXTemplate(message=message, miniprogram=miniprogram)
+    background_tasks.add_task(wx_sender.send, to_users, template_id, new_wx_messages)
+    return "success"
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ message_push/models.py
@@ -0,0 +1,57 @@
+from typing import Optional, List, Dict
+from pydantic import BaseModel
+from enum import Enum
+
+
+class PriorityEnum(Enum):
+    """
+    优先级定义
+    """
+    High = "high"
+    Normal = "none"
+    Low = "low"
+
+
+class EmailModel(BaseModel):
+    """
+    发送邮件消息格式 \n
+    subject: 邮件主题 \n
+    template_name: 模板名称，需要和html文件名保持一致 \n
+    to_users: 接收对象邮箱列表 \n
+    cc_users: cc对象邮箱列表 \n
+    message: 消息模板 \n
+    priority: 优先级（high，none，low）\n
+    """
+    subject: str
+    template_name: str
+    to_users: List[str]
+    cc_users: Optional[List[str]] = None
+    message: Dict
+    priority: Optional[PriorityEnum] = "none"
+
+
+class SMSModel(BaseModel):
+    """
+    发送短信消息格式 \n
+    template_name: 模板名称，需要和平台注册的保持一致 \n
+    to_users: 接收对象手机号列表 \n
+    message: 消息模板 \n
+    """
+    template_name: str
+    to_users: List[str]
+    message: Dict
+
+
+class WechatModel(BaseModel):
+    """
+    发送微信公众号消息格式 \n
+    template_id: 模板id，需要和微信提供的模板id保持一致 \n
+    to_users: 接收对象邮箱列表 \n
+    message: 消息模板 \n
+    miniprogram: 关联小程序 \n
+    """
+    template_id: str
+    to_users: List[str]
+    message: Dict
+    miniprogram: Optional[Dict] = None
+
